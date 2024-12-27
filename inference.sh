a3ef2436f1468469df07953c84c70bf1ee241109
yahavb <ybiran@ananware.systems>
2024-12-27 01:38:45 +0000

    clean

--- inference.sh
+++ inference.sh
@@ -2,4 +2,4 @@
 
 myloc="$(dirname "$0")"
 
-python "$myloc/inference.py" -p "A cat holding a sign that says hello world" -hh 256 -w 256 -m 32 -n 50
+python "$myloc/inference.py" -p "A cat holding a sign that says hello world" -hh 512 -w 512 -m 32 -n 50
